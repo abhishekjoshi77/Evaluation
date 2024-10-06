6f50d75d8773e1b83df23b3fa3d843894016f898
abhishekjoshi77 <94050150+abhishekjoshi77@users.noreply.github.com>
2024-10-06 16:28:30 +0530

    Update question4.js

--- question4.js
+++ question4.js
@@ -1,1 +1,19 @@
+function deepCopy(xyz) {
+  if (typeof xyz !== 'object' || xyz === null) {
+    return xyz; // primitive values or null can be returned as is
+  }
 
+  if (Array.isArray(xyz)) {
+    return xyz.map(deepCopy); // recursively copy each element in the array
+  }
+
+  const copiedObj = {};
+
+  for (const key in xyz) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      copiedObj[key] = deepCopy(obj[key]); // recursively copy each property
+    }
+  }
+
+  return copiedObj;
+}
