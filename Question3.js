a73861173134f785e59b8d3d0ce2fea3c50ec136
abhishekjoshi77 <94050150+abhishekjoshi77@users.noreply.github.com>
2024-10-06 16:14:40 +0530

    Create Question3.js

---
+++ Question3.js
@@ -0,0 +1,10 @@
+function isPalindrome(str) {
+  
+  const clean = str.replace(/[^a-z0-9]/gi, '').toLowerCase();
+  const reverse = clean.split('').reverse().join('');
+  return clean === reverse;
+}
+
+const result = isPalindrome("A man, a plan, a canal, Panama");
+console.log(result);
+// Output: true
