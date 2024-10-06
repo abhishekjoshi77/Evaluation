3324da7cb949220f612cf653ce91025d1181105b
abhishekjoshi77 <94050150+abhishekjoshi77@users.noreply.github.com>
2024-10-06 16:30:25 +0530

    Update question4.js

--- question4.js
+++ question4.js
@@ -1,3 +1,22 @@
+function deepCopy(xyz) {
+  if (typeof xyz !== 'object' || xyz === null) {
+    return xyz; 
+  }
+
+  if (Array.isArray(xyz)) {
+    return xyz.map(deepCopy); 
+
+  const copiedObj = {};
+
+  for (const key in xyz) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      copiedObj[key] = deepCopy(obj[key]);
+    }
+  }
+
+  return copiedObj;
+}
+
 const obj = {
   name: "John",
   address: {
