5853d51a23a0d442d6f0093d10c33a6024eebaa4
abhishekjoshi77 <94050150+abhishekjoshi77@users.noreply.github.com>
2024-10-06 16:39:40 +0530

    Update Question6.js

--- Question6.js
+++ Question6.js
@@ -1,1 +1,14 @@
+function rearrangeFruits(yuppy) {
+  if (yuppy.length < 4) {
+    return yuppy;
+  }
+  const remove = yuppy.splice(-4);
+  yuppy.splice(0,0, ... remove);
+  return yuppy;
+}
+
+const yuppy = ["Apple", "Banana", "Orange", "Mango", "Pineapple", "Grapes", "Strawberry", "Watermelon", "Peach", "Kiwi"];
+
+console.log(rearrangeFruits(yuppy));
+
 
