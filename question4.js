f903d66d4ce3e9519229be694191dec25456d8a3
abhishekjoshi77 <94050150+abhishekjoshi77@users.noreply.github.com>
2024-10-06 17:31:39 +0530

    Update question4.js

--- question4.js
+++ question4.js
@@ -16,3 +16,15 @@ function deepCopy(obj) {
 
     return copy;
 }
+const obj = {
+  name: "John",
+  address: {
+    city: "New York",
+    zip: 10001
+  }
+};
+
+const copy = deepCopy(obj);
+console.log(copy);
+// Output: { name: "John", address: { city: "New York", zip: 10001 } }
+
